799f7e877c6bd191464f9a3bfb7d53ea2d51ffbb
Alexander Toapanta <dev.xandert@gmail.com>
2024-10-26 12:24:42 -0500

    Assing public static

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import compression from "compression";
 import cors from "cors";
+import path from "path";
 
 // Import swagger configuration
 import setupSwagger from "./config/swagger.js";
@@ -16,7 +17,7 @@ const app = express();
 //Middlewares
 app.use(helmet());
 app.use(morgan("combined"));
-app.set("trust proxy", 0);
+app.set("trust proxy", 1);
 app.use(compression());
 app.use(limiter);
 app.use(cors(corsConfig));
