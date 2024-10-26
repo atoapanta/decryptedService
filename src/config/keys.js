ada8c8c5e51a17131e4e4461b4d73fd0ddde5eb8
Alexander Toapanta <dev.xandert@gmail.com>
2024-10-26 12:02:08 -0500

    Update path private key

--- src/config/keys.js
+++ src/config/keys.js
@@ -2,13 +2,10 @@ import fs from "fs";
 import forge from "node-forge";
 import setting from "./setting.js";
 
-const { PRI_KEY_PATH } = setting;
-
-// Public Key PEM
-const priKeyPem = fs.readFileSync(PRI_KEY_PATH, "utf8");
+const { PRIVATE_KEY } = setting;
 
 // Convert public key pem to forge pki
-const privateKey = forge.pki.privateKeyFromPem(priKeyPem);
+const privateKey = forge.pki.privateKeyFromPem(PRIVATE_KEY);
 
 // Export public key
 export { privateKey };
--- src/config/setting.js
+++ src/config/setting.js
@@ -5,6 +5,6 @@ config();
 export default {
   PORT: process.env.PORT || 0,
   ORIGIN_CORS: process.env.ALLOWED_ORIGIN || "*",
-  PRI_KEY_PATH: process.env.PRI_KEY_PATH || "",
+  PRIVATE_KEY: process.env.PRIVATE_KEY || "",
   URL: process.env.URL || `http://localhost:${process.env.PORT}`,
 };
